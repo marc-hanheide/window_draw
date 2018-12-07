6c1256a74d778ffdf2332ee0f7d4d04d8a88f134
Marc Hanheide <marc@hanheide.net>
2018-12-07 01:13:30 +0000

    many fixes

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ listen_port = 8212
 
 # fading out for the client (30 fps), so 1/30 fades in out within a second,
 # 1/300 fades it out in 10 seconds
-client_decay = 1.0 / (30 * 30)
+client_decay = 1.0 / (30 * 10)
 # fading out for the server
-server_decay = 1.0 / (30 * 30)
+server_decay = 1.0 / (30 * 10)
 
 # how much the points wiggle about in pixels
 server_jitter = 0.05
