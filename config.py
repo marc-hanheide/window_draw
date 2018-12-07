31694f107f4fa85f6cfd1c5ddefa4adc9235655c
Marc Hanheide <marc@hanheide.net>
2018-12-07 08:07:23 +0000

    more zoom fixes

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ listen_port = 8212
 
 # fading out for the client (30 fps), so 1/30 fades in out within a second,
 # 1/300 fades it out in 10 seconds
-client_decay = 1.0 / (30 * 10)
+client_decay = 1.0 / (30 * 30)
 # fading out for the server
-server_decay = 1.0 / (30 * 10)
+server_decay = 1.0 / (30 * 20)
 
 # how much the points wiggle about in pixels
-server_jitter = 0.05
+server_jitter = 0.03
 
 config = {
     'width': 600,
